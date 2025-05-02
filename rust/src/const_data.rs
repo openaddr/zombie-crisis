a2a9b1499ad35bb0f5aaeabac96389bbbfd31111
openaddr <openaddr@foxmail.com>
2025-05-02 17:24:29 +0800

    feat(const_data): 增加 Direction 枚举并更新场景数据
    - 在 const_data.rs 中添加 Direction 枚举类，用于表示方向
    - 实现从方向向量获取 Direction 枚举值的方法
    - 更新 main_scene.tscn 中的瓦片地图数据

--- rust/src/const_data.rs
+++ rust/src/const_data.rs
@@ -1,8 +1,39 @@
+use godot::prelude::Vector2;
 
+/// 方向朝向, 默认朝下
+#[derive(Default,PartialEq)]
 pub enum Direction {
     Up,
+    #[default]
     Down,
     Left,
     Right,
 }
 
+impl Direction {
+    /// 根据方向向量, 确定正在移动的方向; 如果方向向量为零向量(静止), 则返回None
+    /// 优先考虑水平方向
+    pub fn from_direction_vector_vector(direction_vector: &Vector2) -> Option<Self> {
+        if direction_vector.x > 0.0 {
+            Some(Direction::Right)
+        } else if direction_vector.x < 0.0 {
+            Some(Direction::Left)
+        } else if direction_vector.y > 0.0 {
+            Some(Direction::Down)
+        } else if direction_vector.y < 0.0 {
+            Some(Direction::Up)
+        } else {
+            None // 如果没有移动向量,则返回None,代表静止
+        }
+    }
+
+    pub fn as_str(&self) -> &'static str {
+        match *self {
+            // *self 有 Direction 类型
+            Direction::Up => "up",
+            Direction::Down => "down",
+            Direction::Left => "left",
+            Direction::Right => "right",
+        }
+    }
+}
--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,37 +1,44 @@
+use crate::const_data::Direction;
 use godot::classes::*;
 use godot::prelude::*;
-use crate::const_data::Direction;
 
 #[derive(GodotClass)]
 #[class(init,base=CharacterBody2D)]
-struct Player{
+struct Player {
     base: Base<CharacterBody2D>,
 
     #[init(val = 100.0)]
     speed: f32,
+
+    direction: Direction,
+
+    #[init(node = "AnimatedSprite2D")]
+    animator: OnReady<Gd<AnimatedSprite2D>>,
+}
+
+impl Player {
+    
+    /// 根据方向改变动画
+    fn change_animation_by_direction(&mut self) {
+        self.animator.set_animation(self.direction.as_str());
+    }
 }
 
 #[godot_api]
 impl ICharacterBody2D for Player {
     fn physics_process(&mut self, _: f64) {
         let direction = Input::singleton().get_vector("ui_left", "ui_right", "ui_up", "ui_down");
-        godot_print!("direction {:?}",direction);
         let v = direction * self.speed; // 矢量速度
         self.base_mut().set_velocity(v);
         self.base_mut().move_and_slide();
 
-        let Vector2 { x, y } = direction; // todo 根据方向设置动画
-        if x>0.0 { 
-            Some("right")
-        } else if x<0.0 { 
-            Some("left")
-        } else if y>0.0 { 
-            Some("up")
-        } else if y<0.0 { 
-            Some("down")
-        } else {
-            None
-        };
-        
+        let direction_state = Direction::from_direction_vector_vector(&direction); // 获取移动方向
+
+        if let Some(direct) = direction_state { // 如果有移动方向
+            if direct != self.direction { // 如果移动方向发生改变
+                self.direction = direct; // 设置当前方向
+                self.change_animation_by_direction();
+            }
+        }
     }
 }
