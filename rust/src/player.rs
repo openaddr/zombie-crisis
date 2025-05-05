f1cd35944978302d5f4ab9afbd6e74c10705c781
ning <openaddr@foxmail.com>
2025-05-05 14:08:20 +0800

    refactor(player): 重构物理处理逻辑并添加地图边界限制
    - 移动地图边界限制的设置，以优化性能
    - 添加玩家位置的边界检查逻辑
    -调整输入方向的处理，为后续边界限制做准备
    - 移除不必要的 unwrap 调用，提高代码安全性

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -33,7 +33,29 @@ impl ICharacterBody2D for Player {
         godot_print!("Player ready");
     }
     fn physics_process(&mut self, _: f64) {
-        let direction = Input::singleton().get_vector("ui_left", "ui_right", "ui_up", "ui_down");
+        let gd = global();
+        let gd = gd.bind();
+        let world_size = gd.world_size.expect("地图大小值尚未初始化");
+        self.camera.set_limit(Side::TOP, 0);  // 每帧设置世界边界, todo 太浪费性能, 是否能优化
+        self.camera.set_limit(Side::LEFT, 0);
+        self.camera.set_limit(Side::BOTTOM, world_size.y);
+        self.camera.set_limit(Side::RIGHT, world_size.x);
+        
+        godot_print!("world_size {:?}", world_size);
+        
+        let mut direction = Input::singleton().get_vector("ui_left", "ui_right", "ui_up", "ui_down");
+        
+        // 如果当前位置在地图边界外, 则将对应的移动向量设置为零
+        // let position = self.base().get_position(); // 当前位置
+        // if position.x <= 0.0 {
+        //     
+        // }
+        // if position.x < 0.0 || position.x > world_size.x as f32 ||
+        //     position.y < 0.0 || position.y > world_size.y as f32 {
+        //     direction.x = 0.0;
+        //     direction.y = 0.0;
+        // }
+        // 
         let v = direction * self.speed; // 矢量速度
         self.base_mut().set_velocity(v);
         self.base_mut().move_and_slide();
@@ -52,12 +74,6 @@ impl ICharacterBody2D for Player {
             self.animator.pause();
         }
 
-        let gd = global();
-        let gd = gd.bind();
-        self.camera.set_limit(Side::TOP, 0);  // 每帧设置世界边界, todo 太浪费性能, 是否能优化
-        self.camera.set_limit(Side::LEFT, 0);
-        self.camera.set_limit(Side::BOTTOM, gd.world_size.unwrap().y);
-        self.camera.set_limit(Side::RIGHT, gd.world_size.unwrap().x);
 
         // todo 根据地图大小限制人物移动范围, 而不是通过空气墙
         
